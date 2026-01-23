cbe9be457f05218602d95024226506726f824d72
AngeloMoontanercpu <montanerangelo97@gmail.com>
2026-01-22 21:20:57 -0600

    Excercise_02

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-int main(int argc, char *argv)
-{
-    printf("Hello world!\n");
+int main(){
+
+//Insertar variables.
+    float lado;
+    float area;
+    float volumen;
+
+    printf("Insertar valor:");
+    scanf("%f", &lado);
+
+    area = lado * lado;
+    volumen = lado * lado * lado;
+
+//Imprimir resultados.
+    printf("Resultado de Area: %.2f\n", area);
+    printf("Resultado de volumen %.2f\n", volumen);
+
     return 0;
 }
